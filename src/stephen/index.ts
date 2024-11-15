d500137c5884f81928921bbe5b29510e5436b1c1
stormymayday <stormymayday7@gmail.com>
2024-11-15 11:30:39 -0800

    Linked List Constructor

---
+++ src/stephen/index.ts
@@ -0,0 +1,15 @@
+class Node {
+    data: number;
+    next: Node | null;
+    constructor(data: number, next: Node | null = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    head: Node | null;
+    constructor() {
+        this.head = null;
+    }
+}
