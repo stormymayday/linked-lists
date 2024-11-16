bce72cb0ce9085e152ac4d7afe619b6b3424aaa0
stormymayday <stormymayday7@gmail.com>
2024-11-15 23:01:16 -0800

    removeAt method

--- src/stephen/index.ts
+++ src/stephen/index.ts
@@ -104,4 +104,17 @@ class LinkedList {
         }
         return null;
     }
+
+    removeAt(index: number): void {
+        if (index < 0 || !this.head) {
+            return;
+        }
+        if (index === 0) {
+            this.head = this.head.next;
+            return;
+        }
+        const prev = this.getAt(index - 1);
+        if (!prev || !prev.next) return;
+        prev.next = prev.next.next;
+    }
 }
